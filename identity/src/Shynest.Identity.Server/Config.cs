dbf96d6162d4a7be2b5e1f21e9f3874e7157256e
Maksim Shyla <maksim.shila@gmail.com>
2023-12-18 15:46:04 +0300

    Used Duende IdentityServer

---
+++ identity/src/Shynest.Identity.Server/Config.cs
@@ -0,0 +1,39 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Shynest.Identity.Server;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new(name: "build-your-head-api", displayName: "Build Your Head Api")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new()
+            {
+                ClientId = "build-your-head-client",
+                AllowedGrantTypes = GrantTypes.Code,
+                ClientSecrets = { new Secret("TODO_hide_this".Sha256()) },
+                RedirectUris = { "http://localhost:3000/" },
+                PostLogoutRedirectUris = { "http://localhost:3000/" },
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "build-your-head-api"
+                }
+            }
+        };
+}
---
+++ identity/src/Shynest.Identity.Server/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace Shynest.Identity.Server.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
